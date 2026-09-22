e6d094c6ee0a3082b420812265b75103ac8b2db2
wputr <2582003@unai.edu>
2026-09-22 19:32:26 +0700

    Menambahkan input data kelayakan ujian

--- src/KelayakanUjian.java
+++ src/KelayakanUjian.java
@@ -4,3 +4,15 @@ public class KelayakanUjian {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        // Input data dari pengguna
+        System.out.print("Kehadiran (%) : ");
+        int kehadiran = scanner.nextInt();
+
+        System.out.print("Nilai tugas   : ");
+        int nilaiTugas = scanner.nextInt();
+
+        System.out.print("Dispensasi     : ");
+        boolean dispensasi = scanner.nextBoolean();
+    }
+}
