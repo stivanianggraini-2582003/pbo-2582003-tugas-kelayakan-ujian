7627b062df2511b2db22fb0d7bfda23e81e49414
wputr <2582003@unai.edu>
2026-09-22 19:36:09 +0700

    Menambahkan logika kelayakan ujian

--- src/KelayakanUjian.java
+++ src/KelayakanUjian.java
@@ -14,5 +14,23 @@ public class KelayakanUjian {
 
         System.out.print("Dispensasi     : ");
         boolean dispensasi = scanner.nextBoolean();
+        // Menghitung tiga versi logika
+        boolean a = kehadiran >= 75 && nilaiTugas >= 60 || dispensasi;
+
+        boolean b = (kehadiran >= 75 && nilaiTugas >= 60)
+                || dispensasi;
+
+        boolean c = kehadiran >= 75
+                && (nilaiTugas >= 60 || dispensasi);
+
+        // Negasi
+        boolean tidakDispensasi = !dispensasi;
+
+        // Short-circuit dengan post-increment
+        int cek = 0;
+
+        boolean x = (kehadiran >= 75) && (cek++ >= 0);
+
+        boolean y = (nilaiTugas >= 60) || (cek++ >= 0);
     }
 }
